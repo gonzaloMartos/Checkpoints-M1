2c82dbb29f5d920dc2bbd8b7125aa2ab4c4db747
gonzaloMartos <gonzalomartos2002@gmail.com>
2023-10-03 17:42:50 -0300

    CP04-1 completo

--- CP04/challenge/CheckPoint-04-I.js
+++ CP04/challenge/CheckPoint-04-I.js
@@ -53,25 +53,75 @@ if(años >= 18){
 
 // Escribí tu código acá abajo:
 
+const numeroDivisible = 5;
+
+if(numeroDivisible % 3 === 0 && numeroDivisible % 5 === 0){
+  console.log("es divisible por 3 y 5");
+}else{
+  console.log("no es divisible por 3 y 5");
+} 
+
 // Ejercicio 6: Utilizar una declaración if para determinar si una letra es una vocal.
 
 // Escribí tu código acá abajo:
+
+const letra = "a";
+
+if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+  console.log("es vocal");
+}else{
+  console.log("no es vocal");
+}
 
 // Ejercicio 7: Utilizar una declaración if para determinar si una persona puede votar (mayor de 18 y ciudadano).
 
 // Escribí tu código acá abajo:
 
+const edad = 18;
+const ciudadania = "argentina"
+
+if(edad >= 18 && ciudadania === "argentina"){
+  console.log("puede votar");
+}else{
+  console.log("no puede votar");
+}
+
 // Ejercicio 8: Utilizar una declaración if para determinar si un número es positivo o cero.
 
 // Escribí tu código acá abajo:
+
+const num5 = 2;
+
+if(num5 > 0){
+  console.log("este numero es positivo");
+}else if(num5 === 0){
+  console.log("este numero es cero");
+}
 
 // Ejercicio 9: Utilizar una declaración if para determinar si un número es mayor que 10 y menor que 20.
 
 // Escribí tu código acá abajo:
 
+const num6 = 15;
+
+if(num6 > 10 && num6 < 20){
+  console.log("este numero es mayor que 10 y menor que 20");
+}else{
+  console.log("este numero no entra dentro del rango anterior");
+}
+
 // Ejercicio 10: Utilizar una declaración if para determinar si una persona puede conducir (mayor de 16 y tiene licencia).
 
 // Escribí tu código acá abajo:
+
+const edad2 = 16;
+const licencia = "si";
+
+if(edad2 >= 16 && licencia === "si"){
+  console.log("puede conducir");
+}else{
+  console.log("no puede conducir");
+}
 
 // Exporta las variables para que estén disponibles para otros módulos
 module.exports = {
